726e519a6c5bfec8c49f3f2c3c82233b8c7eaadc
Samuel Kelemen <SCKelemen@users.noreply.github.com>
2017-06-11 21:24:23 -0500

    meh

--- models/todoItem.go
+++ models/todoItem.go
@@ -7,8 +7,10 @@ import (
 type Todo struct {
 	ID		int32
 	Title		string
+	Description	string
+	Due		time.Time
 	Created		time.Time
-	CreatedBy	int32
+	Created		int32
 	Assigned	[]int32
 	Status		Status
 	Deleted		bool
@@ -29,3 +31,30 @@ type Status struct {
 	Time	time.Time
 	User	int32
 }
+
+
+type Comment struct {
+	ID		int32
+	Parent		int32
+	ParentType	ObjectType
+	Text		string
+	Created		time.Time
+	Creator		int32
+}
+
+type Reaction struct {
+	ID		int32
+	Parent		int32
+	ParentType	ObjectType	
+}
+
+type ObjectType int 
+
+const (
+	Todo ObjectType = iota
+	TodoList 
+	Status
+	Comment
+	Reaction
+)
+
